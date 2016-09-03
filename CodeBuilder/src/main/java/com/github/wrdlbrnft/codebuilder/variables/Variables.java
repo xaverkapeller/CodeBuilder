d2ffb15c72db2f16f5c5d277615fe1456cd5a8de
Xaver Kapeller <xaver.kapeller@gmail.com>
2016-09-03 15:17:49 +0200

    Added factory methods for pre/post increment and decrement operations.

--- CodeBuilder/src/main/java/com/github/wrdlbrnft/codebuilder/variables/Variables.java
+++ CodeBuilder/src/main/java/com/github/wrdlbrnft/codebuilder/variables/Variables.java
@@ -1,6 +1,8 @@
 package com.github.wrdlbrnft.codebuilder.variables;
 
 import com.github.wrdlbrnft.codebuilder.annotations.Annotation;
+import com.github.wrdlbrnft.codebuilder.code.Block;
+import com.github.wrdlbrnft.codebuilder.code.BlockWriter;
 import com.github.wrdlbrnft.codebuilder.code.CodeElement;
 import com.github.wrdlbrnft.codebuilder.elements.names.Names;
 import com.github.wrdlbrnft.codebuilder.types.Type;
@@ -29,11 +31,59 @@ public class Variables {
                 .build();
     }
 
+    public static CodeElement postIncrement(Variable variable) {
+        return new PostOperatorImpl(variable, "++");
+    }
+
+    public static CodeElement preIncrement(Variable variable) {
+        return new PreOperatorImpl(variable, "++");
+    }
+
+    public static CodeElement postDecrement(Variable variable) {
+        return new PostOperatorImpl(variable, "--");
+    }
+
+    public static CodeElement preDecrement(Variable variable) {
+        return new PreOperatorImpl(variable, "--");
+    }
+
     public static CodeElement set(Variable variable, CodeElement value) {
         return new SetElementImpl(variable, value);
     }
 
     public static Variable stub(Type type, String name) {
         return new VariableImpl(Collections.<Modifier>emptySet(), Collections.<Annotation>emptyList(), type, Names.fixedName(name), false, false);
+    }
+
+    private static class PostOperatorImpl extends BlockWriter {
+
+        private final Variable mVariable;
+        private final String mOperator;
+
+        private PostOperatorImpl(Variable variable, String operator) {
+            mVariable = variable;
+            mOperator = operator;
+        }
+
+        @Override
+        protected void write(Block block) {
+            block.append(mVariable).append(mOperator);
+        }
+    }
+
+    private static class PreOperatorImpl extends BlockWriter {
+
+        private final Variable mVariable;
+        private final String mOperator;
+
+        private PreOperatorImpl(Variable variable, String operator) {
+            mVariable = variable;
+            mOperator = operator;
+        }
+
+        @Override
+        protected void write(Block block) {
+            block.append(mOperator).append(mVariable);
+        }
     }
 }
